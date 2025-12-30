80bfc0110244858aeef9170b9b4e2a8d0570355b
misaki_fujishiro <Misaki.Fujishiro@nttdata.com>
2025-12-30 17:09:03 +0900

    sphinxテンプレ更新

--- source/conf.py
+++ source/conf.py
@@ -35,4 +35,5 @@ language = 'ja'
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
